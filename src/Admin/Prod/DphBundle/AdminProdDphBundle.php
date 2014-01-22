c2786e2983425904417a95d2b48cff3bcb8845f8
Radek Horáček <radoshordos@gmail.com>
2014-01-22 19:41:10 +0100

    namespace Admin\Prod\DphBundle\Entity;

---
+++ src/Admin/Prod/DphBundle/AdminProdDphBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Admin\Prod\DphBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminProdDphBundle extends Bundle
+{
+}
---
+++ src/Admin/Prod/DphBundle/Entity/Dph.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Admin\Prod\DphBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dph
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Dph
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="visible", type="boolean")
+     */
+    private $visible;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="multiple", type="decimal")
+     */
+    private $multiple;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=8)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set visible
+     *
+     * @param boolean $visible
+     * @return Dph
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    /**
+     * Get visible
+     *
+     * @return boolean 
+     */
+    public function getVisible()
+    {
+        return $this->visible;
+    }
+
+    /**
+     * Set multiple
+     *
+     * @param string $multiple
+     * @return Dph
+     */
+    public function setMultiple($multiple)
+    {
+        $this->multiple = $multiple;
+
+        return $this;
+    }
+
+    /**
+     * Get multiple
+     *
+     * @return string 
+     */
+    public function getMultiple()
+    {
+        return $this->multiple;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Dph
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
