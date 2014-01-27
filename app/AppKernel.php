d5a2002714272b809b1286f8917501774d69b0b6
Radek Horáček <radoshordos@gmail.com>
2014-01-27 21:30:22 +0100

    add Twitter Bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Kutny\TracyBundle\KutnyTracyBundle(),
-            new Admin\Prod\DphBundle\AdminProdDphBundle(),
-            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle()
+            new Admin\Prod\DphBundle\AdminProdDphBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
