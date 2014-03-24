631136c4a9ae2c6adf22e298569786e785903bb7
Radek Horáček <radoshordos@gmail.com>
2014-03-24 12:14:07 +0100

    small fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Kutny\TracyBundle\KutnyTracyBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new FOS\UserBundle\FOSUserBundle(),
